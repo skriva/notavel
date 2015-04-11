31b22862189345608a204cf6a37ee1c7f2724d44
Paulo Ragonha <paulo@ragonha.me>
2015-04-10 23:17:56 -0700

    yeah! now we got hot module replacement!

--- app/index.js
+++ app/index.js
@@ -23,6 +23,8 @@ app.on('ready', function() {
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
+  mainWindow.webContents.executeJavaScript('require("./bootstrap-jsx"); require("./ui/index.jsx");');
+
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
